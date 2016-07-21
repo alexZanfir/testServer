6744369a7662d89ec04f53ff28e7b78afb73dbf0
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 21:21:41 +0200

    commit init

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Server started on port ' + PORT + '!');
+});
+
+// test 1
+
+console.log('test begins'),
+console.time('timer');
+setTimeout(function(){
+   console.timeEnd('timer');
+   console.log('first task sent has finished');
+},1000)
+
+console.log('second task sent has finished ');
+
