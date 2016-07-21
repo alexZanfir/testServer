9ed5149024faf407624b2e1317efbd3ae1692fea
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 21:40:02 +0200

    test

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var middleware = require('./middleware.js');
 
-app.use(middleware.logger);
+app.get('/', function (req, res) {
+	res.send(console.log('test begins'),
+console.time('timer');
+setTimeout(function(){
+   console.timeEnd('timer');
+   console.log('first task sent has finished');
+},1000)
 
-app.get('/', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us');
+console.log('second task sent has finished '););
 });
-
 
 app.use(express.static(__dirname + '/public'));
 
@@ -19,12 +22,5 @@ app.listen(PORT, function () {
 
 // test 1
 
-console.log('test begins'),
-console.time('timer');
-setTimeout(function(){
-   console.timeEnd('timer');
-   console.log('first task sent has finished');
-},1000)
 
-console.log('second task sent has finished ');
 
